a4772c1c78eefc0d7f47d899049078533e4cf046
ThariqHifzh <thoriqhifzh@gmail.com>
2024-10-24 15:07:08 +0700

    pengaturan-website dll

---
+++ angkatan3_compro.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2024 at 10:05 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `angkatan3_compro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `general_setting`
+--
+
+CREATE TABLE `general_setting` (
+  `id` int(11) NOT NULL,
+  `website_name` varchar(50) NOT NULL,
+  `website_link` varchar(50) DEFAULT NULL,
+  `website_phone` varchar(16) NOT NULL,
+  `website_email` varchar(50) NOT NULL,
+  `website_address` text NOT NULL,
+  `twitter_link` varchar(100) DEFAULT NULL,
+  `fb_link` varchar(100) DEFAULT NULL,
+  `ig_link` varchar(100) DEFAULT NULL,
+  `linkedin_link` varchar(100) DEFAULT NULL,
+  `youtube_link` varchar(100) DEFAULT NULL,
+  `logo` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `general_setting`
+--
+
+INSERT INTO `general_setting` (`id`, `website_name`, `website_link`, `website_phone`, `website_email`, `website_address`, `twitter_link`, `fb_link`, `ig_link`, `linkedin_link`, `youtube_link`, `logo`, `created_at`, `updated_at`) VALUES
+(3, 'Me and Myself', 'https://getbootstrap.com/docs/5.3/getting-started/', '0654678678', 'sayaaa@gmail.com', 'Jl. Qertyuiop, Jakarta-Indonesia', NULL, NULL, NULL, NULL, NULL, 'pngwing.com (1).png', '2024-10-24 02:54:32', '2024-10-24 07:42:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `email`, `password`, `foto`, `created_at`, `updated_at`) VALUES
+(18, 'Lahh Kocakk', 'fufufafa@gmail.com', '728348f63167a287d2509edf88d74dbf79c44480', '', '2024-10-23 03:54:16', '2024-10-23 03:58:11'),
+(27, 'Saya', 'saya@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', 'php.png', '2024-10-23 08:14:30', '2024-10-23 08:19:42'),
+(29, 'Duarr', 'duarr@gmail.com', '12345678', 'foto.png', '2024-10-24 01:04:06', '2024-10-24 01:04:06'),
+(30, 'Admin', 'admin@gmail.com', '12345678', 'images.png', '2024-10-24 01:05:32', '2024-10-24 01:05:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `general_setting`
+--
+ALTER TABLE `general_setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `general_setting`
+--
+ALTER TABLE `general_setting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
