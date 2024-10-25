c2d1bd95c5d8454eaea96fc774e4dbdb72aded54
ThariqHifzh <thoriqhifzh@gmail.com>
2024-10-25 14:58:31 +0700

    admin ke portofolio

--- angkatan3_compro.sql
+++ angkatan3_compro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2024 at 10:05 AM
+-- Generation Time: Oct 25, 2024 at 09:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -49,7 +49,74 @@ CREATE TABLE `general_setting` (
 --
 
 INSERT INTO `general_setting` (`id`, `website_name`, `website_link`, `website_phone`, `website_email`, `website_address`, `twitter_link`, `fb_link`, `ig_link`, `linkedin_link`, `youtube_link`, `logo`, `created_at`, `updated_at`) VALUES
-(3, 'Me and Myself', 'https://getbootstrap.com/docs/5.3/getting-started/', '0654678678', 'sayaaa@gmail.com', 'Jl. Qertyuiop, Jakarta-Indonesia', NULL, NULL, NULL, NULL, NULL, 'pngwing.com (1).png', '2024-10-24 02:54:32', '2024-10-24 07:42:45');
+(3, 'Me and Myself', 'https://getbootstrap.com/docs/5.3/getting-started/', '0654678678', 'sayaaa@gmail.com', 'Jl. Qertyuiop, Jakarta-Indonesia', NULL, NULL, NULL, NULL, NULL, 'pslogo.png', '2024-10-24 02:54:32', '2024-10-25 06:23:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instruktur`
+--
+
+CREATE TABLE `instruktur` (
+  `id` int(11) NOT NULL,
+  `nama_instruktur` varchar(100) NOT NULL,
+  `jurusan_instruktur` varchar(40) NOT NULL,
+  `fb_link` varchar(50) DEFAULT NULL,
+  `ig_link` varchar(50) DEFAULT NULL,
+  `twitter_link` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `foto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `instruktur`
+--
+
+INSERT INTO `instruktur` (`id`, `nama_instruktur`, `jurusan_instruktur`, `fb_link`, `ig_link`, `twitter_link`, `created_at`, `updated_at`, `foto`) VALUES
+(4, 'Albert Einstein', 'Physics science', NULL, NULL, NULL, '2024-10-25 04:08:02', '2024-10-25 04:32:52', 'download.png'),
+(6, 'Marcus Aurelius', 'Faculty of Law', NULL, NULL, NULL, '2024-10-25 04:09:50', '2024-10-25 06:54:47', 'ma.png'),
+(8, 'Ibnu Sina', 'Faculty of Medicine', NULL, NULL, NULL, '2024-10-25 04:35:52', '2024-10-25 07:05:33', 'ibnsina.png'),
+(9, 'J.R. Oppenheimer', 'Physics and Chemistry', NULL, NULL, NULL, '2024-10-25 07:00:22', '2024-10-25 07:00:22', 'op.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_profile`
+--
+
+CREATE TABLE `kategori_profile` (
+  `id` int(11) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori_profile`
+--
+
+INSERT INTO `kategori_profile` (`id`, `nama_kategori`, `created_at`, `updated_at`) VALUES
+(1, 'Latar Belakang', '2024-10-25 07:40:27', '2024-10-25 07:40:27'),
+(2, 'Visi dan Misi', '2024-10-25 07:40:27', '2024-10-25 07:40:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profil`
+--
+
+CREATE TABLE `profil` (
+  `id` int(11) NOT NULL,
+  `id_kategori` varchar(11) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `slug` text NOT NULL,
+  `isi` text NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -88,6 +155,24 @@ ALTER TABLE `general_setting`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `instruktur`
+--
+ALTER TABLE `instruktur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori_profile`
+--
+ALTER TABLE `kategori_profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -102,6 +187,24 @@ ALTER TABLE `user`
 --
 ALTER TABLE `general_setting`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `instruktur`
+--
+ALTER TABLE `instruktur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `kategori_profile`
+--
+ALTER TABLE `kategori_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
